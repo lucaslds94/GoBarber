33ef501becb0d4681680af8c65af8e215b42ee7a
Lucas Lima <lucas.eng.lds@gmail.com>
2020-05-25 22:17:43 -0300

    [Backend] Adicionar vaidação de data nos agendamentos

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,22 +1,41 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (req, res) => {
-  const { provider , date } = req.body;
+  const { provider, date, isEqual } = req.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return res
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
 
   const appointment = {
     id: uuid(),
     provider,
-    date
-  }
+    date:parsedDate,
+  };
 
-  appointments.push(appointment)
+  appointments.push(appointment);
 
   return res.json(appointment);
-})
+});
 
 export default appointmentsRouter;
