a79773af7b8a5b685a4594f1704ea28a5f27b9d8
Lucas Lima <lucas.eng.lds@gmail.com>
2020-05-30 18:10:57 -0300

    [Frontend] Adicionar login através do contexto

--- web/src/context/AuthContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-interface AuthContextData {
-  name: string;
-}
-
-const AuthContext = createContext<AuthContextData | null>({} as AuthContextData);
-
-export default AuthContext;
