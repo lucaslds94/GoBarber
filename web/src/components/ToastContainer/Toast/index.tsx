7801240fdd79ad254c37e72c94a741604a20f148
Lucas Lima <lucas.eng.lds@gmail.com>
2020-06-16 22:26:29 -0300

    [Web] Adicionar animações de toast

--- web/src/components/ToastContainer/Toast/index.tsx
+++ web/src/components/ToastContainer/Toast/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+
 import {
   FiAlertCircle,
   FiCheckCircle,
@@ -12,6 +13,7 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+  style: object;
 }
 
 const icons = {
@@ -20,7 +22,7 @@ const icons = {
   success: <FiCheckCircle size={24} />,
 };
 
-const Toast: React.FC<ToastProps> = ({ message }) => {
+const Toast: React.FC<ToastProps> = ({ message, style }) => {
   const { removeToast } = useToast();
 
   useEffect(() => {
@@ -34,7 +36,11 @@ const Toast: React.FC<ToastProps> = ({ message }) => {
   }, [removeToast, message.id]);
 
   return (
-    <Container type={message.type} hasDescription={!!message.description}>
+    <Container
+      type={message.type}
+      style={style}
+      hasDescription={!!message.description}
+    >
       {icons[message.type || 'info']}
 
       <div>
