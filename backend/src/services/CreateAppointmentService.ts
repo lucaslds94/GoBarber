0c3a7f45d47a17a62bae4236654990704f6157f5
Lucas Lima <lucas.eng.lds@gmail.com>
2020-06-21 17:23:32 -0300

    [Backend] Adicionar criação de usuário

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -6,23 +6,25 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import { startOfHour } from 'date-fns';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider }: Request): Promise<Appointment> {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(date);
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      date,
+    );
 
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
