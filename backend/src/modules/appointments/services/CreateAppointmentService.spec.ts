27591e6b0eb3d8882afa0f2ee0a9c6b6ffd7e4f6
Lucas Lima <lucas.eng.lds@gmail.com>
2020-07-09 12:53:07 -0300

    [Backend] Add repositório fake de appointments

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,6 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
 test('sum of two numbers', () => {
   expect(1 + 2).toBe(3);
 });
