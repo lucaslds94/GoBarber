8384349c1f353d48fec55eb814255c47aef8e47f
Lucas Lima <lucas.eng.lds@gmail.com>
2020-07-28 20:08:29 -0300

    [Backend] Iniciar teste de availability

---
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,43 @@
+// import AppError from '../../../shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+
+describe('ListProvidersMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService();
+  });
+  it('Should be able to list the month availability from providers', async () => {
+    await fakeAppointmentsRepository.create({
+      date: new Date(2020, 9, 13, 8, 0, 0),
+      provider_id: 'user',
+    });
+
+    await fakeAppointmentsRepository.create({
+      date: new Date(2020, 9, 13, 11, 0, 0),
+      provider_id: 'user',
+    });
+    await fakeAppointmentsRepository.create({
+      date: new Date(2020, 9, 14, 9, 0, 0),
+      provider_id: 'user',
+    });
+
+    const availability = await listProviderMonthAvailability.execute({
+      user_id: 'user',
+      month: 10,
+      year: 2020,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 12, availability: true },
+        { day: 13, availability: false },
+        { day: 14, availability: false },
+        { day: 14, availability: false },
+      ]),
+    );
+  });
+});
