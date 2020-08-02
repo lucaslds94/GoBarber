6738b817936d502588886331854abb7e3346f03a
Lucas Lima <lucas.eng.lds@gmail.com>
2020-08-02 15:14:50 -0300

    [Backend] Add notification schema and repository

---
+++ backend/src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,27 @@
+import { getMongoRepository, MongoRepository } from 'typeorm';
+
+import ICreateNotificationDTO from '../../../dtos/ICreateNotificationDTO';
+import INotificationsRepository from '../../../repositories/INotificationsRepository';
+
+import Notification from '../schemas/Notification';
+
+class NotificationsRepository implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({ content, recipient_id });
+
+    this.ormRepository.save(notification);
+
+    return notification;
+  }
+}
+
+export default NotificationsRepository;
